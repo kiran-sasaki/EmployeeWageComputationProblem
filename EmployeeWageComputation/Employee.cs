14345493ff1c2d917c2a08b56d8699e769e77598
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-09 15:39:53 +0530

    Addded Employee Attendance Check Funtion

--- EmployeeWageComputation/Employee.cs
+++ EmployeeWageComputation/Employee.cs
@@ -1,10 +1,24 @@
 ﻿namespace EmployeeWageComputation
 {
-    class Program
+    class Employee
     {
         public static void Main(string[] args)
         {
+            //Main
             Console.WriteLine("Welcome to EmployeeWage Computation");
+            //UC1
+            int FullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck == FullTime)
+            {
+                Console.WriteLine("empolyee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+            Console.ReadKey();
         }
     }
 }
